bd21a27e21c424598d930c1ec4488a3ad8ae0302
Albert Sare <thesundaymailman@gmail.com>
2018-11-27 12:28:49 -0800

    part 2 - display list complete

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Captain America' },
+  { id: 12, name: 'Iron Man' },
+  { id: 13, name: 'Dr. Strange' },
+  { id: 14, name: 'Spider-Man' },
+  { id: 15, name: 'Hulk' },
+  { id: 16, name: 'Star-Lord' },
+  { id: 17, name: 'Scarlet Witch' },
+  { id: 18, name: 'Black Panther' },
+  { id: 19, name: 'Thor' },
+  { id: 20, name: 'Ant-Man' }
+];
